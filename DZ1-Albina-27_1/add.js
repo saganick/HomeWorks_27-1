79c9230ff9d5ef1b2cf2f3761b0b31c437592b0e
dibi <133033039+saganick@users.noreply.github.com>
2023-06-05 18:43:53 +0600

    сделала ДЗ -1

---
+++ DZ1-Albina-27_1/add.js
@@ -0,0 +1,45 @@
+
+       //1 Пункт
+// var greetings = prompt('Введите имя и фамилию!')
+// alert('Здраствуйте,' + greetings)
+// console.log(greetings)'
+
+    //2 Пункт
+// var bank = {
+//     address: 'Pushkin 211',
+//     bankName: 'Optima',
+//     weekendWork: false,
+//     amountCash: 5,
+//     workingHours: {
+//         from: '10:00  ',
+//         to: '18:00',
+//     },
+//     breakLunch: {
+//         from: '13:00  ',
+//         to: '14:00',
+//     }
+//
+// }
+// console.log(bank)
+
+
+
+       // 3 Пункт
+
+       // * написать переменную которая хранит объект банковского счета:
+       //     номер счета(строка),
+       //     валюта(строка),
+       //     баланс(число),
+       //     имя владельца(строка),
+       //     заблокирован ли счет (логический тип).
+
+
+
+// var object = {
+//     accountNumber: '346198640',
+//     currency: 'som',
+//     balance: 223345,
+//     ownName: 'Jysan Bank',
+//     blockedAccount: false,
+// }
+//        console.log(object)
